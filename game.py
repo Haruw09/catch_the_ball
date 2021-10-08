e4779600a7f30704a0bbd6b05302866308b5406a
Haruw09 <91204233+Haruw09@users.noreply.github.com>
2021-10-08 11:51:49 +0300

    Update game.py
    вообще убрала функцию click и добавила функцию position, возвращающую true, если курсор попал в шар, и false - если не попал

--- game.py
+++ game.py
@@ -24,11 +24,13 @@ def new_ball():
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
 
-def click():
-    global x1, y1
+def position(x, y):
+    pos = pygame.mouse.get_pos()
     x1 = pos[0]
-    y1 = pos[1]
-    print(x1, y1, r)
+    x2 = pos[1]
+    if (x1 - x) ** 2 + (y1 - y) ** 2 < r ** 2:
+        return True
+    return False
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -40,11 +42,8 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            click()
-            print('Click!')
-            if (x1 - x) ** 2 + (y1 - y) ** 2 < r ** 2:
-                points = points + 1
+            if position(x, y):
+                points += 1
             print(points)
                 
             
